0bd00411bbba25536df441dbf4352c029cbb4a59
Oscar <oscar101019@gmail.com>
2018-03-21 17:13:20 -0700

    Primer Commit

---
+++ app/src/main/java/com/example/oscar/criptomonedas/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.oscar.criptomonedas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button click;
+    public static TextView data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = (Button)findViewById(R.id.btnConsultar);
+        data = (TextView)findViewById(R.id.txtSalida);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetJson proces=new GetJson();
+                proces.execute();
+            }
+        });
+
+    }
+}
